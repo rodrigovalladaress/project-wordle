0f5c54bae51797227ebba9d2bbec0948ab1901b5
Rodrigo <dim3trodon@gmail.com>
2026-03-12 16:15:04 +0000

    feat: add guess input

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  /**
+   *
+   * @param {React.ChangeEvent<HTMLInputElement>} e
+   */
+  function handleChange(e) {
+    setGuess(e.target.value.toUpperCase());
+  }
+
+  /**
+   *
+   * @param {React.FormEvent<HTMLFormElement>} e
+   */
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(guess);
+
+    setGuess("");
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={handleChange}
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        required
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
